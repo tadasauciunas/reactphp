560eae1e2516c6d4dca3a2288b7122a4e6fc07c6
auciutad <tadas.auciunas@gmail.com>
2017-08-27 23:31:59 +0300

    Initial commit

---
+++ EventLoop/Server.php
@@ -0,0 +1,24 @@
+<?php
+
+include '../vendor/autoload.php';
+
+use React\EventLoop\Factory as EventLoopFactory;
+use React\Stream\ReadableResourceStream;
+
+$loop = EventLoopFactory::create();
+
+$timer = $loop->addPeriodicTimer(0.0001, function () use (&$counter, $loop) {
+    echo " ";
+});
+
+$stream = new ReadableResourceStream(fopen('HelloWorld.txt', 'r'), $loop, 1);
+
+$stream->on('data', function ($data) {
+    echo $data;
+});
+
+$stream->on('close', function () use ($timer, $loop) {
+    $loop->cancelTimer($timer);
+});
+
+$loop->run();
